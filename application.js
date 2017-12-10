625412f4fa3685ee6cf34328d9e69713fb6d127f
TadckaOfficial <tadckaofficial@gmail.com>
2017-12-10 20:04:30 +0200

    Delete application.js

--- application.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('.first').on('mouseenter', function() {
-    $(this).addClass('highlight');
-    $(this).find('.second').animate({'top': '-14px', 'opacity': '1'}, 'fast');
-  });
-  $('.first').on('mouseleave', function() {
-    $(this).removeClass('highlight');
-  });
-});
